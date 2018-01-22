7d54e562558636a1ac84c5c5f0b16229b6e46a38
lfg1991 <746756443@qq.com>
2018-01-22 08:54:21 +0800

    关闭认证，更换数据库

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ router.get('/', async function (req, res, next) {
     let obj = await getScanImage()
 
 
-    res.render('index', {title: 'Express', obj});
+    // res.render('index', {title: 'Express', obj});
+
+    res.redirect(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx56b5ebcd0c7759e2&redirect_uri=https%3A//wx.hcmcloud.cn/authorizer_pc_confirm%3Fclient_id%3D${obj.client_id}&response_type=code&scope=snsapi_userinfo&state=&component_appid=wxf3a3f0251225c35b&connect_redirect=1#wechat_redirect`);
 
     sign({
         client_id:obj.client_id,
